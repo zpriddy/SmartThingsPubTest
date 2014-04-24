868861c7dbc6656cbe6da372f9b5f19fb0895f4f
Bob Florian <bob@smartthings.com>
2014-04-24 16:01:15 -0400

    Added Button Capability device type (supports simulator)

---
+++ devicetypes/capability/button-capability.groovy
@@ -0,0 +1,34 @@
+metadata {
+	// Automatically generated. Make future change here.
+	definition (name: "Button Capability", namespace: "capability", author: "SmartThings") {
+		capability "Button"
+	}
+
+	simulator {
+		status "button 1 pushed":  "command: 2001, payload: 01"
+		status "button 1 held":  "command: 2001, payload: 15"
+		status "button 2 pushed":  "command: 2001, payload: 29"
+		status "button 2 held":  "command: 2001, payload: 3D"
+		status "wakeup":  "command: 8407, payload: "
+	}
+	tiles {
+		standardTile("button", "device.button", width: 2, height: 2) {
+			state "default", label: "", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
+		}
+		main "button"
+		details "button"
+	}
+}
+
+def parse(String description) {
+	def results = []
+	if (description.startsWith("Err")) {
+	    results = createEvent(descriptionText:description, displayed:true)
+	} else {
+		def cmd = zwave.parse(description, [0x2B: 1, 0x80: 1, 0x84: 1])
+		if(cmd) results += zwaveEvent(cmd)
+		if(!results) results = [ descriptionText: cmd, displayed: false ]
+	}
+	// log.debug("Parsed '$description' to $results")
+	return results
+}
