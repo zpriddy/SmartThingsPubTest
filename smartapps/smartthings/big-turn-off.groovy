9b90240b9564b879dcd44522fbcaa5713de5a125
bflorian <bob@smartthings.com>
2014-05-01 09:32:27 -0400

    Added definition to Big Turn OFF

--- smartapps/smartthings/big-turn-off.groovy
+++ smartapps/smartthings/big-turn-off.groovy
@@ -3,6 +3,15 @@
  *
  *  Author: SmartThings
  */
+definition(
+    name: "Big Turn OFF",
+    namespace: "smartthings",
+    author: "SmartThings",
+    description: "Turn your lights off when the SmartApp is tapped or activated",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet@2x.png"
+)
 preferences {
 	section("When I touch the app, turn off...") {
 		input "switches", "capability.switch", multiple: true
