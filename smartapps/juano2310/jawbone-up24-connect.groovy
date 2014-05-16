66f5acfd32e0d77949cf0cf638ac1d1fb3dc9c33
Bob Florian <bflorian@gmail.com>
2014-05-16 12:10:38 -0400

    Moved jawbone UP service manager from internal to reviewers

--- smartapps/juano2310/jawbone-up24-connect.groovy
+++ smartapps/juano2310/jawbone-up24-connect.groovy
@@ -9,7 +9,7 @@ definition(
 	namespace: "juano2310",
 	author: "Juan Pablo Risso",
 	description: "Connect your Jawbone UP to SmartThings",
-	category: "SmartThings Internal",
+	category: "Reviewers",
 	iconUrl: "http://thinkmakelab.com/images/upsmall.png",
 	iconX2Url: "http://thinkmakelab.com/images/upbig.png",
 	oauth: true
