5d42d2eba8679c9b5388606a7fe4a21805683302
Bob Florian <bob@smartthings.com>
2014-06-13 15:20:11 -0400

    Replaced custom attributes with Sleep Sensor and Step Sensor capabilities in Jawbone User device type

--- devicetypes/juano2310/jawbone-user.groovy
+++ devicetypes/juano2310/jawbone-user.groovy
@@ -12,72 +12,70 @@ metadata {
 		capability "Refresh"
 		capability "Polling"
         capability "Button"
-        
-        attribute "sleeping", "string"
-        attribute "steps", "string"
-        attribute "goal", "string"        
+        capability "Sleep Sensor"
+        capability "Step Sensor"         
 	}
 
-        simulator {
-                status "sleeping": "sleeping: 1"
-                status "not sleeping": "sleeping: 0"
-        }
+    simulator {
+        status "sleeping": "sleeping: 1"
+        status "not sleeping": "sleeping: 0"
+    }
 
-        tiles {
-                standardTile("sleeping", "device.sleeping", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false) {
-                        state("sleeping", label: "Sleeping", icon:"st.Bedroom.bedroom12", backgroundColor:"#ffffff")
-                        state("not sleeping", label: "Awake", icon:"st.Health & Wellness.health12", backgroundColor:"#79b821")
-                }
-                standardTile("steps", "device.steps", width: 2, height: 2, canChangeIcon: false, canChangeBackground: false) {
-                        state("steps", label: '${currentValue} Steps', icon:"st.Health & Wellness.health11", backgroundColor:"#ffffff")                     
-				}
-                standardTile("goal", "device.goal", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false,  decoration: "flat") {
-                        state("goal", label: '${currentValue} Steps', icon:"st.Health & Wellness.health5", backgroundColor:"#ffffff")
-                }                
-                standardTile("refresh", "device.steps", inactiveLabel: false, decoration: "flat") {
-						state "default", action:"polling.poll", icon:"st.secondary.refresh"
-				}
-                main "steps"
-				details(["steps", "goal", "sleeping", "refresh"])
+    tiles {
+        standardTile("sleeping", "device.sleeping", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false) {
+            state("sleeping", label: "Sleeping", icon:"st.Bedroom.bedroom12", backgroundColor:"#ffffff")
+            state("not sleeping", label: "Awake", icon:"st.Health & Wellness.health12", backgroundColor:"#79b821")
         }
+        standardTile("steps", "device.steps", width: 2, height: 2, canChangeIcon: false, canChangeBackground: false) {
+            state("steps", label: '${currentValue} Steps', icon:"st.Health & Wellness.health11", backgroundColor:"#ffffff")                     
+        }
+        standardTile("goal", "device.goal", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false,  decoration: "flat") {
+            state("goal", label: '${currentValue} Steps', icon:"st.Health & Wellness.health5", backgroundColor:"#ffffff")
+        }                
+        standardTile("refresh", "device.steps", inactiveLabel: false, decoration: "flat") {
+            state "default", action:"polling.poll", icon:"st.secondary.refresh"
+        }
+        main "steps"
+        details(["steps", "goal", "sleeping", "refresh"])
+    }
 }
 
 def generateSleepingEvent(boolean sleeping) {
-        log.debug "Here in generateSleepingEvent!"
-        def value = formatValue(sleeping)
-        def linkText = getLinkText(device)
-        def descriptionText = formatDescriptionText(linkText, sleeping)
-        def handlerName = getState(sleeping)
+    log.debug "Here in generateSleepingEvent!"
+    def value = formatValue(sleeping)
+    def linkText = getLinkText(device)
+    def descriptionText = formatDescriptionText(linkText, sleeping)
+    def handlerName = getState(sleeping)
 
-        def results = [
-                name: "sleeping",
-                value: value,
-                unit: null,
-                linkText: linkText,
-                descriptionText: descriptionText,
-                handlerName: handlerName
-        ]
+    def results = [
+        name: "sleeping",
+        value: value,
+        unit: null,
+        linkText: linkText,
+        descriptionText: descriptionText,
+        handlerName: handlerName
+    ]
 
-        sendEvent (results)
-        
-        log.debug "Generating Sleep Event: ${results}"        
-        
-        
-        def results2 = [
-                name: "button",
-                value: "held",
-                unit: null,
-                linkText: linkText,
-                descriptionText: "${linkText} button was pressed",
-                handlerName: "buttonHandler",
-                data: [buttonNumber: 1],
-                isStateChange: true
-        ] 
-        
-       
-        log.debug "Generating Button Event: ${results2}"
-        
-        sendEvent (results2)
+    sendEvent (results)
+
+    log.debug "Generating Sleep Event: ${results}"        
+
+
+    def results2 = [
+        name: "button",
+        value: "held",
+        unit: null,
+        linkText: linkText,
+        descriptionText: "${linkText} button was pressed",
+        handlerName: "buttonHandler",
+        data: [buttonNumber: 1],
+        isStateChange: true
+    ] 
+
+
+    log.debug "Generating Button Event: ${results2}"
+
+    sendEvent (results2)
 }
 
 
@@ -98,22 +96,22 @@ def getMemberId () {
 }
 
 private String formatValue(boolean sleeping) {
-        if (sleeping)
-            return "sleeping"
-        else
-            return "not sleeping"
+    if (sleeping)
+    	return "sleeping"
+    else
+        return "not sleeping"
 }
 
 private formatDescriptionText(String linkText, boolean sleeping) {
-        if (sleeping)
-            return "$linkText is sleeping"
-        else
-            return "$linkText is not sleeping"
+    if (sleeping)
+    	return "$linkText is sleeping"
+    else
+        return "$linkText is not sleeping"
 }
 
 private getState(boolean sleeping) {
-        if (sleeping)
-            return "sleeping"
-        else
-            return "not sleeping"
+    if (sleeping)
+    	return "sleeping"
+    else
+        return "not sleeping"
 }
