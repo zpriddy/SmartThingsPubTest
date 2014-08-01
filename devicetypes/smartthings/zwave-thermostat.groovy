6cf9f29be8733d3509a2acd3b9d9caef06ae6211
Bob Florian <bflorian@gmail.com>
2014-08-01 09:09:27 -0400

    Added Relative Humidity Measurement capability  to Z-Wave thermostat

--- devicetypes/smartthings/zwave-thermostat.groovy
+++ devicetypes/smartthings/zwave-thermostat.groovy
@@ -3,6 +3,7 @@ metadata {
 	definition (name: "Z-Wave Thermostat", namespace: "smartthings", author: "SmartThings") {
 		capability "Actuator"
 		capability "Temperature Measurement"
+		capability "Relative Humidity Measurement"
 		capability "Thermostat"
 		capability "Configuration"
 		capability "Polling"
