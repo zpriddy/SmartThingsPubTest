3be2cedda814a327dc77de3fa3743054ee18fff1
Todd Wackford <todd@wackford.net>
2014-09-17 08:45:50 -0400

    Submitting 'Update TCP Connected Lights Service Manager and device type' for publication

---
+++ devicetypes/wackford/tcp-bulb-v2.groovy
@@ -0,0 +1,151 @@
+/**
+ *  TCP Bulbs.groovy
+ *
+ *  Author: todd@wackford.net
+ *  Date: 2014-03-07
+ *
+ *
+ *****************************************************************
+ *                       Changes
+ *****************************************************************
+ *
+ *  Change 1:	2014-03-10
+ *				Documented Header
+ *
+ *  Change 2:	2014-03-15
+ *				Fixed bug where we weren't coming on when changing 
+ *				levels down.
+ *
+ *  Change 3:   2014-04-02 (lieberman)	
+ *              Changed sendEvent() to createEvent() in parse()
+ *
+ *  Change 4:	2014-04-12 (wackford)
+ *				Added current power usage tile 
+ *
+ *  Change 5:	2014-09-14 (wackford)
+ *				a. Changed createEvent() to sendEvent() in parse() to
+ *				   fix tile not updating.
+ *				b. Call IP checker for DHCP environments from refresh. Parent
+ *				   service manager has method to call every 5 minutes too.
+ *
+ *****************************************************************
+ *                       Code
+ *****************************************************************
+ */
+ // for the UI
+metadata {
+	definition (name: "TCP Bulb V2", namespace: "Wackford", author: "Todd Wackford") {
+		capability "Switch"
+		capability "Polling"
+		capability "Power Meter"
+		capability "Refresh"
+		capability "Switch Level"
+
+		command "levelUp"
+		command "levelDown"
+        command "on"
+        command "off"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "on", label:'${name}', action:"off", icon:"st.Lighting.light14", backgroundColor:"#79b821"
+			state "off", label:'${name}', action:"on", icon:"st.Lighting.light14", backgroundColor:"#ffffff"
+		}
+		controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false) {
+			state "level", action:"switch level.setLevel"
+		}
+		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+		valueTile("level", "device.level", inactiveLabel: false, decoration: "flat") {
+			state "level", label: 'Level ${currentValue}%'
+		}
+        standardTile("lUp", "device.switchLevel", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
+            state "default", action:"levelUp", icon:"st.illuminance.illuminance.bright"
+        }
+        standardTile("lDown", "device.switchLevel", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
+            state "default", action:"levelDown", icon:"st.illuminance.illuminance.light"
+        }
+        valueTile( "power", "device.power", inactiveLabel: false, decoration: "flat") {
+            state "power", label: '${currentValue}', unit:"", backgroundColor:"#ffffff"
+        }
+
+		main(["switch"])
+		details(["switch", "lUp", "lDown", "levelSliderControl", "level" , "power", "refresh" ])
+	}
+}
+
+// parse events into attributes
+def parse(description) {
+	log.debug "parse() - $description"
+	def results = []
+
+	if (description?.name && description?.value)
+	{
+		results << createEvent(name: "${description?.name}", value: "${description?.value}")
+	}
+}
+
+// handle commands
+def on() {
+	log.debug "Executing 'on'"
+    sendEvent(name:"switch",value:on)
+	parent.on(this)
+}
+
+def off() {
+	log.debug "Executing 'off'"
+    sendEvent(name:"switch",value:off)
+	parent.off(this)
+}
+
+def levelUp() {
+	def level = device.latestValue("level") as Integer ?: 0
+    
+    level+= 10
+    
+    if ( level > 99 )
+    	level = 99
+    
+    setLevel(level)
+}
+
+def levelDown() {
+	def level = device.latestValue("level") as Integer ?: 0
+    
+    level-= 10
+    
+	if ( level <  1 )
+    	level = 1
+    
+    setLevel(level)
+}
+
+def setLevel(value) {
+	def level = value as Integer
+
+    if (( level > 0 ) && ( level < 100 ))
+    	on()
+    else
+    	off()
+    
+    sendEvent( name: "level", value: level )
+    sendEvent( name: "switch.setLevel", value:level )
+	parent.setLevel( this, level )
+}
+
+def poll() {
+	log.debug "Executing poll()"
+	parent.poll(this)
+}
+
+def refresh() {
+	log.debug "Executing refresh()"
+    parent.updateGatewayIP()
+	parent.poll(this)
+}
---
+++ smartapps/wackford/1-tcp-bulbs-service-manager-v2.groovy
@@ -0,0 +1,500 @@
+/*
+ *  TCP-Bulbs-Service-Manager.groovy
+ *
+ *  Author: todd@wackford.net
+ *
+ ******************************************************************************
+ *                        Setup Namespace & OAuth
+ ******************************************************************************
+ *
+ * Namespace:			"wackford"
+ *
+ * OAuth:				"Enabled"
+ *
+ ******************************************************************************
+ *                                 Changes
+ ******************************************************************************
+ *
+ *  Change 1:	2014-03-06
+ *					Initial Release
+ *
+ *  Change 2:	2014-03-15
+ *					a. Documented Header
+ *					b. Fixed on()/off() during level changes			
+ *					c. Minor code cleanup and UI changes
+ *
+ *  Change 3:   2014-04-02 (lieberman) 	
+ *                  a. Added RoomGetCarousel to poll()
+ * 					b. Added checks in locationHandler() to sync ST status with TCP status
+ *
+ *  Change 4:   2014-05-02 (twackford) 	
+ *                  a. Added current power usage functionality
+ *
+ *  Change 5:	2014-09-14 (twackford)
+ *					a. Fixed on/off tile update
+ *					b. Fixed var type issues with power calculations
+ *					c. Added IP checker for DHCP environments
+ *
+ ******************************************************************************
+ *                                   Code
+ ******************************************************************************
+ */
+
+
+// Automatically generated. Make future change here.
+definition(
+    name: "1-TCP-Bulbs-Service-Manager-V2",
+    namespace: "wackford",
+    author: "Todd Wackford",
+    description: "TCP Bulbs with current power consumption tile/reporting",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
+)
+
+preferences {
+	page(name:"Gateway", title: "Begin Connected by TCP Device Discovery", content: "discoverGateway", nextPage: "bulbDiscovery", install: false)
+	page(name:"bulbDiscovery", title:"TCP Device Setup", content:"bulbDiscovery", refreshTimeout:5)
+}
+
+def updateGatewayIP() {
+	log.debug "Checking to see if IP changed"
+    sendHubCommand(new physicalgraph.device.HubAction("lan discovery urn:greenwavereality-com:service:gop:1", physicalgraph.device.Protocol.LAN))
+}
+
+def discoverGateway() {
+	log.debug "In discoverGateway"
+	if(canInstallLabs()) {
+		state.subscribe = false
+		state.gateway = []
+
+		if(!state.subscribe) {
+			subscribe(location, null, locationHandler, [filterEvents:false])
+			state.subscribe = true
+		}
+
+		//send out the search for the gateway, we'll pick up response in locationEvt
+		sendHubCommand(new physicalgraph.device.HubAction("lan discovery urn:greenwavereality-com:service:gop:1", physicalgraph.device.Protocol.LAN))
+
+		bulbDiscovery()
+	}
+	else
+	{
+		def upgradeNeeded = """Before you can participate in SmartThings Labs we need to update your hub.
+
+Please contact our support team at labs@smartthings.com and tell them you want access to SmartThings Labs!"""
+
+
+		return dynamicPage(name:"Gateway", title:"Upgrade needed!", nextPage:"", install:false, uninstall: true) {
+			section {
+				paragraph "$upgradeNeeded"
+			}
+		}
+
+	}
+}
+
+private bulbDiscovery() {
+	log.debug "In bulbDiscovery"
+
+	def data = "<gwrcmds><gwrcmd><gcmd>RoomGetCarousel</gcmd><gdata><gip><fields>name,power,control,status</fields></gip></gdata></gwrcmd></gwrcmds>"
+
+	def qParams = [
+		cmd: "GWRBatch",
+		data: "${data}",
+		fmt: "xml"
+	]
+
+	def cmd = "/gwr/gop.php?" + toQueryString(qParams)
+
+	// Check for bulbs 
+	state.bulbs = state.bulbs ?: [:]
+
+
+	if (state.bulbs.size() == 0) {
+		sendCommand(cmd)
+	}
+
+	def options = bulbsDiscovered() ?: []
+	def numFound = options.size() ?: 0
+
+	return dynamicPage(name:"bulbDiscovery", title:"Discovering TCP Bulbs!", nextPage:"", refreshInterval: 3, install:true, uninstall: true) {
+		section("Please wait while we look for TCP Bulbs: ${numFound} discovered") {
+			input "selectedBulbs", "enum", required:false, title:"Tap here to select bulbs to setup", multiple:true, options:options
+		}
+	}
+}
+
+def installed() {
+	unschedule()
+	initialize();
+}
+
+def updated() {
+	unschedule()
+	initialize();
+}
+
+def initialize() {
+    schedule("0 0/5 * * * ?", updateGatewayIP)
+	if (selectedBulbs) {
+		addBulbs()
+	}
+}
+
+def addBulbs() {
+	log.debug " in addBulbs"
+
+	def bulbs = getBulbs()
+	def name  = "Dimmer Switch"
+	def deviceFile = "TCP Bulb V2"
+
+	selectedBulbs.each { dni ->
+		def d = getChildDevice(dni)
+		if(!d) {
+			def newBulb = bulbs.find { (it.value.id) == dni }
+
+			d = addChildDevice("wackford", deviceFile, dni, null, [name: "${name}", label: "${newBulb?.value.name}", completedSetup: true])
+
+			if (newBulb?.value.state == "1")   //set ST device state as we find the TCP state to be
+				d.on()
+			else
+				d.off()
+
+			d.setLevel(newBulb?.value.level)
+
+		} else {
+			log.debug "We already added this device"
+		}
+	}
+}
+
+def getBulbs()
+{
+	state.bulbs = state.bulbs ?: [:]
+}
+
+Map bulbsDiscovered() {
+	def bulbs =  getBulbs()
+	log.debug bulbs
+	def map = [:]
+	if (bulbs instanceof java.util.Map) {
+		bulbs.each {
+			def value = "${it?.value?.name}"
+			def key = it?.value?.id
+			map["${key}"] = value
+		}
+	} else { //backwards compatable
+		bulbs.each {
+			def value = "${it?.name}"
+			def key = it?.id
+			map["${key}"] = value
+		}
+	}
+	map
+}
+
+def calculateCurrentPowerUse(deviceCapability, usePercentage) {
+	log.debug "In calculateCurrentPowerUse()"
+    
+    log.debug "deviceCapability: ${deviceCapability}"
+    log.debug "usePercentage: ${usePercentage}"
+    
+    def calcPower = usePercentage * 1000
+    def reportPower = calcPower.round(1) as String
+    reportPower += " Watt(s)"
+    
+    log.debug "report power = ${reportPower}"
+    
+    return reportPower
+}
+
+def locationHandler(evt) {
+	log.debug "In locationHandler()"
+
+	def description = evt.description
+	def hub = evt?.hubId
+
+	def parsedEvent = parseEventMessage(description)
+	parsedEvent << ["hub":hub]
+
+	if (parsedEvent.ssdpTerm?.contains("urn:greenwavereality-com:service:gop:1"))
+	{
+		//stuff the gateway data
+		state.gateway = []
+		state.gateway = ([ 	'ip' 		: parsedEvent.ip,
+                            'port'    	: "0050", //tcp returns zeros for some reason, dunno
+							'type'   	: 'gateway',
+							'dni'     	: parsedEvent.ssdpUSN,
+							'path'		: parsedEvent.ssdpPath
+		])
+        
+        log.debug "parsedEvent.ip = $parsedEvent.ip"
+        //log.debug "Gateway State Data: "
+
+	}
+
+	if (parsedEvent.headers && parsedEvent.body)
+	{
+    	log.debug( "in headers + body" )
+		def headerString = new String(parsedEvent.headers.decodeBase64())
+		def bodyString = new String(parsedEvent.body.decodeBase64())
+
+		def type = (headerString =~ /Content-Type:.*/) ? (headerString =~ /Content-Type:.*/)[0] : null
+
+		def body = new XmlSlurper().parseText(bodyString)
+
+		def devices = []
+		def bulbIndex = 1
+		def lastRoomName = null
+
+		body.gwrcmd.gdata.gip.'*'.each({
+
+			if (it.name() == 'room')
+			{
+				def roomName = it.name.text()
+				it.'*'.each({
+					if ( roomName != lastRoomName ) {
+						bulbIndex = 1 //reset counter for names
+					}
+					if (it.name() == 'device')
+					{
+						if (state.bulbs == null) {
+							state.bulbs = [:]
+						}
+                        
+                        def bulbDid =             it.did.text() as String
+		         		def bulbState =           it.state.text()
+                		def bulbLevel =           it.level.text()
+                        def bulbPowerCapability = it.other.bulbpower.text()// as Integer                       
+                        def bulbPowerPercentage = it.power.text() as Double
+                        def bulbPower = calculateCurrentPowerUse(bulbPowerCapability, bulbPowerPercentage)
+                        
+                        def theBulb = getChildDevice( bulbDid )
+
+						if ( theBulb ) {
+							log.debug( "bulb exists in state and the bulb's state is ${bulbState}" )
+							def currentBulbState = theBulb.currentValue("switch")
+							def currentBulbLevel = theBulb.currentValue( "level" ) as Integer
+                            sendEvent( bulbDid, [name: "power", value: bulbPower] )
+                            
+							if ( currentBulbState == "on" && bulbState == "0" ) {
+								log.debug( "ST thinks the bulb is on, but TCP says the bulb is off" )
+								sendEvent( bulbDid, [name: "switch",value:"off"] )
+                                sendEvent( bulbDid, [name: "power", value: "0 Watt(s)"] )
+							}
+                            
+							if ( currentBulbState == "off" && bulbState == "1" ) {
+								log.debug( "ST thinks the bulb is off, but TCP says the bulb is on" )
+								sendEvent( bulbDid, [name: "switch",value:"on"] )
+                                sendEvent( bulbDid, [name: "power", value: bulbPower] )
+							}
+                            
+							if ( currentBulbLevel != bulbLevel ) {
+								log.debug( "ST thinks the bulb level is ${currentBulbLevel} but TCP says the level is ${bulbLevel}" )
+								sendEvent( bulbDid, [name: "level", value: bulbLevel] )
+								sendEvent( bulbDid, [name: "switch.setLevel", value:bulbLevel] )
+								sendEvent( bulbDid, [name: "power", value: bulbPower] )
+							}
+						} else {
+							state.bulbs[it.did.text()] = [id 	: it.did.text(),
+								                          name 	: "${roomName} ${it.name.text()} ${bulbIndex}",
+								                          state	: bulbState,
+								                          level : bulbLevel,
+                                                          power : bulbPower ]
+							lastRoomName = roomName
+							bulbIndex++
+                        }
+					}
+				});
+			}
+		})
+	}
+}
+
+private def parseEventMessage(Map event) {
+	//handles  attribute events
+	return event
+}
+
+private def parseEventMessage(String description) {
+	def event = [:]
+	def parts = description.split(',')
+	parts.each { part ->
+		part = part.trim()
+
+		if (part.startsWith('devicetype:')) {
+			def valueString = part.split(":")[1].trim()
+			event.devicetype = valueString
+		}
+		else if (part.startsWith('mac:')) {
+			def valueString = part.split(":")[1].trim()
+			if (valueString) {
+				event.mac = valueString
+			}
+		}
+		else if (part.startsWith('networkAddress:')) {
+			def valueString = part.split(":")[1].trim()
+			if (valueString) {
+				event.ip = valueString
+                log.debug "event.ip: ${event.ip}"
+			}
+		}
+		else if (part.startsWith('deviceAddress:')) {
+			def valueString = part.split(":")[1].trim()
+			if (valueString) {
+				event.port = valueString
+			}
+		}
+		else if (part.startsWith('ssdpPath:')) {
+			def valueString = part.split(":")[1].trim()
+			if (valueString) {
+				event.ssdpPath = valueString
+			}
+		}
+		else if (part.startsWith('ssdpUSN:')) {
+			part -= "ssdpUSN:"
+			def valueString = part.trim()
+			if (valueString) {
+				event.ssdpUSN = valueString
+			}
+		}
+		else if (part.startsWith('ssdpTerm:')) {
+			part -= "ssdpTerm:"
+			def valueString = part.trim()
+			if (valueString) {
+				event.ssdpTerm = valueString
+			}
+		}
+		else if (part.startsWith('headers')) {
+			part -= "headers:"
+			def valueString = part.trim()
+			if (valueString) {
+				event.headers = valueString
+			}
+		}
+		else if (part.startsWith('body')) {
+			part -= "body:"
+			def valueString = part.trim()
+			if (valueString) {
+				event.body = valueString
+			}
+		}
+	}
+	event
+}
+
+private sendCommand(data)
+{
+	def deviceNetworkId = state.gateway.ip + ":" + state.gateway.port
+    log.debug "Device date = $data"
+
+	sendHubCommand(new physicalgraph.device.HubAction("""GET $data HTTP/1.1\r\nHOST: $deviceNetworkId\r\n\r\n""", physicalgraph.device.Protocol.LAN, "${deviceNetworkId}"))
+}
+
+/**************************************************************************
+ Child Device Call In Methods
+ **************************************************************************/
+def on(childDevice) {
+	log.debug "Got On request from child device"
+
+	def dni = childDevice.device.deviceNetworkId
+
+	def data = "<gip><version>1</version><token>1234567890</token><did>${dni}</did><value>1</value></gip>"
+
+	def qParams = [
+		cmd: "DeviceSendCommand",
+		data: "${data}",
+		fmt: "xml"
+	]
+
+	def cmd = "/gwr/gop.php?" + toQueryString(qParams)
+
+	sendCommand(cmd) 
+    
+}
+
+def off(childDevice) {
+	log.debug "Got Off request from child device"
+
+	def dni = childDevice.device.deviceNetworkId
+
+	def data = "<gip><version>1</version><token>1234567890</token><did>${dni}</did><value>0</value></gip>"
+
+	def qParams = [
+		cmd: "DeviceSendCommand",
+		data: "${data}",
+		fmt: "xml"
+	]
+    
+    sendEvent( dni, [name: "power", value: "0 Watt(s)"] )
+
+	def cmd = "/gwr/gop.php?" + toQueryString(qParams)
+	sendCommand(cmd)
+}
+
+def setLevel(childDevice, value) {
+	log.debug "Got setLevel request from child device"
+
+	def dni = childDevice.device.deviceNetworkId
+
+	def data = "<gip><version>1</version><token>1234567890</token><did>${dni}</did><value>${value}</value><type>level</type></gip>"
+
+	def qParams = [
+		cmd: "DeviceSendCommand",
+		data: "${data}",
+		fmt: "xml"
+	]
+
+	def cmd = "/gwr/gop.php?" + toQueryString(qParams)
+
+	sendCommand(cmd)
+    
+    poll()
+}
+
+def poll(childDevice) {
+	log.debug "In poll()"
+    def data = "<gwrcmds><gwrcmd><gcmd>RoomGetCarousel</gcmd><gdata><gip><fields>name,power,control,status</fields></gip></gdata></gwrcmd></gwrcmds>"
+
+	def qParams = [
+		cmd: "GWRBatch",
+		data: "${data}",
+		fmt: "xml"
+	]
+
+	def cmd = "/gwr/gop.php?" + toQueryString(qParams)
+	sendCommand(cmd)
+}
+
+/******************************************************************************
+ Helper Methods
+ ******************************************************************************/
+String toQueryString(Map m) {
+	return m.collect { k, v -> "${k}=${URLEncoder.encode(v.toString())}" }.sort().join("&")
+}
+
+def debugEvent(message, displayEvent) {
+	def results = [
+		name: "appdebug",
+		descriptionText: message,
+		displayed: displayEvent
+	]
+	log.debug "Generating AppDebug Event: ${results}"
+	sendEvent (results)
+}
+
+private Boolean canInstallLabs()
+{
+	return hasAllHubsOver("000.011.00603")
+}
+
+private Boolean hasAllHubsOver(String desiredFirmware)
+{
+	return realHubFirmwareVersions.every { fw -> fw >= desiredFirmware }
+}
+
+private List getRealHubFirmwareVersions()
+{
+	return location.hubs*.firmwareVersionString.findAll { it }
+}
