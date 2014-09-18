b7fc814832719f0bb0ceb08408ad5b1f3e84e089
Jesse O'Neill-Oine <jesse@smartthings.com>
2014-09-18 10:48:54 -0500

    Remove extra space in description. SMAR-91

--- smartapps/smartthings/smart-nightlight.groovy
+++ smartapps/smartthings/smart-nightlight.groovy
@@ -8,7 +8,7 @@ definition(
     name: "Smart Nightlight",
     namespace: "smartthings",
     author: "SmartThings",
-    description: "Turns on lights when it's dark and motion is detected.  Turns lights off when it becomes light or some time after motion ceases.",
+    description: "Turns on lights when it's dark and motion is detected. Turns lights off when it becomes light or some time after motion ceases.",
     category: "Convenience",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/light_motion-outlet-luminance.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/light_motion-outlet-luminance@2x.png"
