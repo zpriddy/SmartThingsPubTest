1319e3b515b4d2d77a8e9fb98b0496f95df5cf93
Todd Wackford <todd@wackford.net>
2014-09-19 07:55:15 -0400

    Modifying 'Update TCP Connected Lights Service Manager and device type'

--- devicetypes/wackford/tcp-bulb.groovy
+++ devicetypes/wackford/tcp-bulb.groovy
@@ -1,23 +1,9 @@
 /**
- *  TCP-Bulbs-Device.groovy
+ *  TCP Bulbs.groovy
  *
  *  Author: todd@wackford.net
  *  Date: 2014-03-07
  *
- *****************************************************************
- *     Setup Namespace, capabilities, attributes and commands
- *****************************************************************
- * Namespace:			"wackford"
- *
- * Capabilities:		"polling"
- *						"refresh"
- *						"switch"
- *						"switch level"
- *
- * Custom Attributes:	"none"
- *
- * Custom Commands:		"levelUp"
- *						"levelDown"
  *
  *****************************************************************
  *                       Changes
@@ -30,30 +16,45 @@
  *				Fixed bug where we weren't coming on when changing 
  *				levels down.
  *
+ *  Change 3:   2014-04-02 (lieberman)	
+ *              Changed sendEvent() to createEvent() in parse()
+ *
+ *  Change 4:	2014-04-12 (wackford)
+ *				Added current power usage tile 
+ *
+ *  Change 5:	2014-09-14 (wackford)
+ *				a. Changed createEvent() to sendEvent() in parse() to
+ *				   fix tile not updating.
+ *				b. Call IP checker for DHCP environments from refresh. Parent
+ *				   service manager has method to call every 5 minutes too.
+ *
  *****************************************************************
  *                       Code
  *****************************************************************
  */
-
-
+ // for the UI
 metadata {
-
-	definition(name: "TCP Bulb", namespace: "wackford", author: "Todd Wackford") {
+	definition (name: "TCP Bulb", namespace: "wackford", author: "Todd Wackford") {
 		capability "Switch"
-		capability "Switch Level"
-		capability "Refresh"
 		capability "Polling"
+		capability "Power Meter"
+		capability "Refresh"
+		capability "Switch Level"
 
 		command "levelUp"
 		command "levelDown"
+        command "on"
+        command "off"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
 	}
 
 	tiles {
-		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true ) {
-			state "on", label:'${name}', action:"switch.off", icon:"st.Lighting.light14", backgroundColor:"#79b821"//, nextState:"turningOff"
-			state "off", label:'${name}', action:"switch.on", icon:"st.Lighting.light14", backgroundColor:"#ffffff"//, nextState:"turningOn"
-			//state "turningOn", label:'${name}', icon:"st.Lighting.light14", backgroundColor:"#79b821"
-			//state "turningOff", label:'${name}', icon:"st.Lighting.light14", backgroundColor:"#ffffff"
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "on", label:'${name}', action:"off", icon:"st.Lighting.light14", backgroundColor:"#79b821"
+			state "off", label:'${name}', action:"on", icon:"st.Lighting.light14", backgroundColor:"#ffffff"
 		}
 		controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false) {
 			state "level", action:"switch level.setLevel"
@@ -64,18 +65,20 @@ metadata {
 		valueTile("level", "device.level", inactiveLabel: false, decoration: "flat") {
 			state "level", label: 'Level ${currentValue}%'
 		}
-		standardTile("lUp", "device.switchLevel", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
-			state "default", action:"levelUp", icon:"st.illuminance.illuminance.bright"
-		}
-		standardTile("lDown", "device.switchLevel", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
-			state "default", action:"levelDown", icon:"st.illuminance.illuminance.light"
-		}
+        standardTile("lUp", "device.switchLevel", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
+            state "default", action:"levelUp", icon:"st.illuminance.illuminance.bright"
+        }
+        standardTile("lDown", "device.switchLevel", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
+            state "default", action:"levelDown", icon:"st.illuminance.illuminance.light"
+        }
+        valueTile( "power", "device.power", inactiveLabel: false, decoration: "flat") {
+            state "power", label: '${currentValue}', unit:"", backgroundColor:"#ffffff"
+        }
 
 		main(["switch"])
-		details(["switch", "lUp", "lDown", "levelSliderControl", "level" , "refresh" ])
+		details(["switch", "lUp", "lDown", "levelSliderControl", "level" , "power", "refresh" ])
 	}
 }
-
 
 // parse events into attributes
 def parse(description) {
@@ -84,55 +87,55 @@ def parse(description) {
 
 	if (description?.name && description?.value)
 	{
-		results << sendEvent(name: "${description?.name}", value: "${description?.value}")
+		results << createEvent(name: "${description?.name}", value: "${description?.value}")
 	}
 }
 
 // handle commands
 def on() {
 	log.debug "Executing 'on'"
-	sendEvent(name:"switch",value:on)
+    sendEvent(name:"switch",value:on)
 	parent.on(this)
 }
 
 def off() {
 	log.debug "Executing 'off'"
-	sendEvent(name:"switch",value:off)
+    sendEvent(name:"switch",value:off)
 	parent.off(this)
 }
 
 def levelUp() {
 	def level = device.latestValue("level") as Integer ?: 0
-
-	level+= 10
-
-	if ( level > 99 )
-		level = 99
-
-	setLevel(level)
+    
+    level+= 10
+    
+    if ( level > 99 )
+    	level = 99
+    
+    setLevel(level)
 }
 
 def levelDown() {
 	def level = device.latestValue("level") as Integer ?: 0
-
-	level-= 10
-
+    
+    level-= 10
+    
 	if ( level <  1 )
-		level = 1
-
-	setLevel(level)
+    	level = 1
+    
+    setLevel(level)
 }
 
 def setLevel(value) {
 	def level = value as Integer
 
-	if (( level > 0 ) && ( level < 100 ))
-		on()
-	else
-		off()
-
-	sendEvent( name: "level", value: level )
-	sendEvent( name: "switch.setLevel", value:level )
+    if (( level > 0 ) && ( level < 100 ))
+    	on()
+    else
+    	off()
+    
+    sendEvent( name: "level", value: level )
+    sendEvent( name: "switch.setLevel", value:level )
 	parent.setLevel( this, level )
 }
 
@@ -143,5 +146,6 @@ def poll() {
 
 def refresh() {
 	log.debug "Executing refresh()"
+    parent.updateGatewayIP()
 	parent.poll(this)
 }
