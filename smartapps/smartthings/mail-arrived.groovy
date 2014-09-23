34a2f384764483ac4e58e4c5c808437d83fa84d5
Jesse O'Neill-Oine <jesse@smartthings.com>
2014-09-23 11:08:38 -0500

    Add push notification as an option for mail arrived app. SMAR-89

--- smartapps/smartthings/mail-arrived.groovy
+++ smartapps/smartthings/mail-arrived.groovy
@@ -7,7 +7,7 @@ definition(
     name: "Mail Arrived",
     namespace: "smartthings",
     author: "SmartThings",
-    description: "Send a text when mail arrives in your mailbox using SmartSense Multi on your mailbox door.  Note:  battery life may be impacted in cold climates.",
+    description: "Send a text when mail arrives in your mailbox using a SmartSense Multi on your mailbox door. Note: battery life may be impacted in cold climates.",
     category: "Convenience",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/mail_contact.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/mail_contact@2x.png"
@@ -17,8 +17,9 @@ preferences {
 	section("When mail arrives...") {
 		input "accelerationSensor", "capability.accelerationSensor", title: "Where?"
 	}
-	section("Text me at...") {
-		input "phone1", "phone", title: "Phone number?"
+	section("Notify me...") {
+		input "pushNotification", "bool", title: "Push notification", required: false, defaultValue: "true"
+		input "phone1", "phone", title: "Phone number", required: false
 	}
 }
 
@@ -34,17 +35,24 @@ def updated() {
 def accelerationActiveHandler(evt) {
 	log.trace "$evt.value: $evt, $settings"
 
-	// Don't send a continuous stream of text messages
+	// Don't send a continuous stream of notifications
 	def deltaSeconds = 5
 	def timeAgo = new Date(now() - (1000 * deltaSeconds))
 	def recentEvents = accelerationSensor.eventsSince(timeAgo)
 	log.trace "Found ${recentEvents?.size() ?: 0} events in the last $deltaSeconds seconds"
-	def alreadySentSms = recentEvents.count { it.value && it.value == "active" } > 1
+	def alreadySentNotifications = recentEvents.count { it.value && it.value == "active" } > 1
 
-	if (alreadySentSms) {
-		log.debug "SMS already sent to $phone1 within the last $deltaSeconds seconds"
-	} else {
-		log.debug "$accelerationSensor has moved, texting $phone1"
-		sendSms(phone1, "Mail has arrived!")
+	if (alreadySentNotifications) {
+		log.debug "Notifications already sent within the last $deltaSeconds seconds (phone1: $phone1, pushNotification: $pushNotification)"
+	}
+	else {
+		if (phone1 != null && phone1 != "") {
+			log.debug "$accelerationSensor has moved, texting $phone1"
+			sendSms(phone1, "Mail has arrived!")
+		}
+		if (pushNotification) {
+			log.debug "$accelerationSensor has moved, sending push"
+			sendPush("Mail has arrived!")
+		}
 	}
 }
