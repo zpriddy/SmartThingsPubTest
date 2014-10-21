686badaa287128ac39e1db1895532c8124690fca
Yaima <yaimavaldivia@icloud.com>
2014-10-21 16:14:32 -0700

    Migrated to contact book

--- smartapps/smartthings/notify-me-when-my-hub-goes-offline.groovy
+++ smartapps/smartthings/notify-me-when-my-hub-goes-offline.groovy
@@ -6,8 +6,10 @@
 preferences {
 	page(name: "root", title: "Hub Notification", install: true) {
 	    section("Notification method") {
-	        input "pushNotification", "bool", title: "Push notification", description: "Receive a push notification", required: false
-	        input "phone", "phone", title: "Text message at", description: "Tap to enter phone number", required: false
+            input("recipients", "contact", title: "Send notifications to") {
+                input "pushNotification", "bool", title: "Push notification", description: "Receive a push notification", required: false
+                input "phone", "phone", title: "Text message at", description: "Tap to enter phone number", required: false
+            }
 	    }
 	    section("Choose Hub to Monitor") {
 			input "hub", "hub", multiple: true, title: "Select a hub"
@@ -30,12 +32,16 @@ def processEvent(evt) {
     if (evt.value.contains("inactive")) {
 		def msg = evt.description
 		log.debug "A Hub is inactive, sending message: '$msg', push:$pushNotification, phone:$phone"
-
-		if (pushNotification) {
-			sendPush(msg)
-		}
-		if (phone) {
-			sendSms(phone, msg)
-		}
+        if (location.contactBookEnabled) {
+            sendNotification(msg, recipients)
+        }
+        else {
+            if (pushNotification) {
+                sendPush(msg)
+            }
+            if (phone) {
+                sendSms(phone, msg)
+            }
+        }
     }
 }
