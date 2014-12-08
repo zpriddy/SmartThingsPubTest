280a8559a600a856a10d446d537091439ddee78c
Bob Florian <bob@smartthings.com>
2014-12-08 15:35:19 -0500

    Submitting 'Simulated device types' for publication

---
+++ devicetypes/smartthings/testing/simulated-smoke-alarm.groovy
@@ -0,0 +1,55 @@
+metadata {
+	// Automatically generated. Make future change here.
+	definition (name: "Simulated Smoke Alarm", namespace: "smartthings/testing", author: "SmartThings") {
+		capability "Smoke Detector"
+		capability "Sensor"
+
+		attribute "alarmState", "string"
+        
+        command "smoke"
+        command "test"
+        command "clear"
+	}
+
+	simulator {
+
+	}
+
+	tiles {
+		standardTile("smoke", "device.smoke", width: 2, height: 2) {
+			state("clear", label:"Clear", icon:"st.alarm.smoke.clear", backgroundColor:"#ffffff", action:"smoke")
+			state("detected", label:"Smoke!", icon:"st.alarm.smoke.smoke", backgroundColor:"#e86d13", action:"clear")
+			state("tested", label:"Test", icon:"st.alarm.smoke.test", backgroundColor:"#e86d13", action:"clear")
+		}
+ 		standardTile("smoke", "device.smoke", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'Smoke', action:"smoke"
+		}  
+ 		standardTile("test", "device.smoke", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'Test', action:"test"
+		}
+ 		standardTile("reset", "device.smoke", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'Clear', action:"clear"
+		}
+        main "smoke"
+		details(["smoke", "smoke", "test", "clear"])
+	}
+}
+
+def parse(String description) {
+	
+}
+
+def smoke() {
+	log.debug "smoke()"
+	sendEvent(name: "smoke", value: "detected", descriptionText: "$device.displayName detected smoke!")
+}
+
+def test() {
+	log.debug "test()"
+	sendEvent(name: "smoke", value: "tested", descriptionText: "$device.displayName smoke detector tested")
+}
+
+def clear() {
+	log.debug "clear()"
+	sendEvent(name: "smoke", value: "clear", descriptionText: "$device.displayName smoke detector clear")
+}
